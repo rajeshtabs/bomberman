2155202bbafb24f80275711a16243442bbc47f2e
Rajesh Ranjan <rajeshtabs@aol.com>
2022-01-13 22:10:35 +0530

    Click Event Check

---
+++ script.js
@@ -0,0 +1,50 @@
+const bombs=[];
+
+function addGrid(){
+    const appElement=document.getElementById('app');
+    for(let i=0;i<9;i++){
+        const row=document.createElement('div');
+        for(let j=0;j<9;j++){
+            const index=i*9+j;
+            const column=document.createElement('div');
+            column.style.display='inline-block';
+            column.style.width='60px';
+            column.style.border='1px solid black';
+            column.style.height='60px';
+            column.style.textAlign="center";
+            column.style.verticalAlign='middle';
+            column.setAttribute("index",index);
+
+            column.addEventListener("click",function(){
+                if(bombs.includes(index)){
+                    //column.innerHTML = "";
+                    column.style.backgroundColor="red";
+                   // alert("bomb clicked");
+                }
+                else{
+                    column.style.background="green";
+                }
+            })
+           
+            row.appendChild(column);
+        }
+        appElement.appendChild(row);
+    }
+}
+
+
+function generateBombs(){
+    while(bombs.length<11){
+
+    
+    const randomNum=Math.floor(Math.random() * 100);
+    if(randomNum<81 && !bombs.includes(randomNum)){
+        bombs.push(randomNum);
+    }
+}
+
+}
+
+addGrid();
+generateBombs();
+console.log(bombs);
